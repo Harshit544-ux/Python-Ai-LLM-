50547f6a27edb82d302c1a2c1b6dbfb727496ea7
Harshit <srivastava0798@gmail.com>
2026-04-27 16:30:05 +0530

    feat: add examples of basic Python data types (int, float, bool, string, list, tuple, dict, complex)

---
+++ dataType.py
@@ -0,0 +1,36 @@
+# Integer
+print(42)
+
+# Decimal/Float
+print(3.14)
+
+# Boolean - True or False
+print(True)
+print(False)
+
+# String 
+# In Python, there is no separate character type;
+# even a single letter is treated as a string
+print("Hello, World!")
+print('A')  # This is a string, not a character
+
+
+# complex
+print(2+3j)
+
+
+# List - C language -> Array 
+list =[1,2,3,4,5,6]
+print(list)
+
+# Tuple 
+tuple = [(1,2,3,4,5,6)]
+print(tuple)
+
+#  Dictionary ;- { key : value_pairs }
+dict = {"name":"Harshit","age":22,"city":"Hyderabad"}
+print(dict)
+
+
+
+
