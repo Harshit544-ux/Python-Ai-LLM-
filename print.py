c13a43e5165465d70af01d4d1afb73ed3c519164
Harshit <srivastava0798@gmail.com>
2026-04-27 15:01:43 +0530

    refactor print statements for clarity and consistency

--- print.py
+++ print.py
@@ -4,3 +4,17 @@ print("Harshit srivastava")
 print("Garvit srivastava")
 
 print("Rajan Srivastava")
+
+print("Hello world") 
+
+# print the numbers
+print(7)
+
+# print the float value
+print(7.7)
+
+# print the boolean valur - true or false
+print(True)
+
+# it take multiple values and print them with space
+print("Harshit",7.7,True)
