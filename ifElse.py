d1f9e9885ee25c4f15cfd2d3dcdd8a83c24d7c25
Harshit <srivastava0798@gmail.com>
2026-04-29 23:03:16 +0530

    if else , login program and identation

---
+++ ifElse.py
@@ -0,0 +1,9 @@
+# if else :- 
+# login program and identation
+
+email = input ("Enter your email :")
+password = input("Enter your password :")
+if email == "harshitsri08@gmail.com" and password == "harshit123":
+    print("Login Successfully")
+else:
+    print("Invalid email or password")
