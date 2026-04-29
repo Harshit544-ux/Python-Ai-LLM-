96ff12e73e7de76837bfed2988fdca0eb59399e8
Harshit <srivastava0798@gmail.com>
2026-04-29 17:55:38 +0530

    relational operator

--- operator.py
+++ operator.py
@@ -19,3 +19,19 @@ print(5//2) #integer division :- the actual result is 2.5 but it will return 2 d
 print(5%2)  #modulus operator :- it will return the remainder of the division
  
 print(2**3) #exponentiation operator
+
+
+# ====== Relational Operators ======
+# in relational operators we compare two values and return a boolean value (True or False)
+
+print( 5 > 3) #greater than
+
+print( 5 < 3) #less than
+
+print( 5 >= 3) #greater than or equal to
+
+print( 5 <= 3) #less than or equal to
+
+print( 5 == 3) #equal to
+
+print( 5 != 3) #not equal to
