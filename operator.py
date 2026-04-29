a858eb0c73401582689aee68e2d4269b15b102d4
Harshit <srivastava0798@gmail.com>
2026-04-29 17:36:41 +0530

    creating the operator.py file and learn about arithematic operator

---
+++ operator.py
@@ -0,0 +1,21 @@
+# Operators
+# Arithmetic Operators
+# Relational Operators
+# Logical Operators
+# Assignment Operators
+# Membership Operators
+
+# ====== Arithmetic Operators ====== 
+print(5+3)
+
+print(5-3)
+
+print(5*3)
+
+print(5/3)
+ 
+print(5//2) #integer division :- the actual result is 2.5 but it will return 2 due to the floor division
+ 
+print(5%2)  #modulus operator :- it will return the remainder of the division
+ 
+print(2**3) #exponentiation operator
