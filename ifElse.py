70f3a8c00fd08e428fe1248f1df95025dd0960f6
Harshit <srivastava0798@gmail.com>
2026-05-01 19:42:45 +0530

    modules in python

--- ifElse.py
+++ ifElse.py
@@ -1,67 +1,88 @@
-# 1) if else :- 
-# login program and identation
+# # 1 ) if else :- 
+# # login program and identation
 
-email = input ("Enter your email :")
-password = input("Enter your password :")
+# email = input ("Enter your email :")
+# password = input("Enter your password :")
 
-if email == "harshitsri08@gmail.com" and password == "harshit123":
-    print("Login Successfully")
-else:
-    print("Invalid email or password")
+# if email == "harshitsri08@gmail.com" and password == "harshit123":
+#     print("Login Successfully")
+# else:
+#     print("Invalid email or password")
 
 
-# 2) elif :-
+# # 2 ) elif :-
 
-# take the input 
-email = input("Enter your email: ")
-password = input("Enter your password: ")
+# # take the input 
+# email = input("Enter your email: ")
+# password = input("Enter your password: ")
 
-# check the email and password
-if email == "abc@gmail.com" and password == "abc123":
-    print("Login Successfully")
+# # check the email and password
+# if email == "abc@gmail.com" and password == "abc123":
+#     print("Login Successfully")
 
-elif email == "abc@gmail.com" and password != "abc123":
-    # tell the user
-    print("Invalid password")
-    # taking again input for password
-    password = input("Enter your password again: ")
+# elif email == "abc@gmail.com" and password != "abc123":
+#     # tell the user
+#     print("Invalid password")
+#     # taking again input for password
+#     password = input("Enter your password again: ")
     
-    # password is correct or not
-    if password == "abc123":
-        print("Login Successfully after retry")
-    else:
-        print("Login Failed after retry")
+#     # password is correct or not
+#     if password == "abc123":
+#         print("Login Successfully after retry")
+#     else:
+#         print("Login Failed after retry")
 
-# if email is wrong
-else:
-    print("Invalid email")
-
-
-# min of 3 numbers
-a = int(input("Enter first number :"))
-b = int(input("Enter second number :"))
-c = int(input("Enter third number :"))
-
-if a < b and a < c :
-    print("Minimum number is : " , a)
-elif b < a and b < c:
-    print("Minimum number is : " , b)
-else :
-    print("Minimum number is  :" ,c)
+# # if email is wrong
+# else:
+#     print("Invalid email")
 
 
-# menu driven program
-fnum = int(input("Enter first number :"))
-snum = int(input("Enter second number :"))
+# # min of 3 numbers
+# a = int(input("Enter first number :"))
+# b = int(input("Enter second number :"))
+# c = int(input("Enter third number :"))
 
-# take the input for operation
-op = input("Enter the operation you want to perform : + ,- ,*  =  ")
+# if a < b and a < c :
+#     print("Minimum number is : " , a)
+# elif b < a and b < c:
+#     print("Minimum number is : " , b)
+# else :
+#     print("Minimum number is  :" ,c)
 
-if op == "+" :
-    print("the sum is :" , fnum + snum)
-elif op == "-":
-    print("the difference is :" , fnum - snum)
-elif op == "*":
-    print("the product is :" ,fnum * snum)
-else :
-    print("Invalid operation")
+
+# # menu driven program
+# fnum = int(input("Enter first number :"))
+# snum = int(input("Enter second number :"))
+
+# # take the input for operation
+# op = input("Enter the operation you want to perform : + ,- ,*  =  ")
+
+# if op == "+" :
+#     print("the sum is :" , fnum + snum)
+# elif op == "-":
+#     print("the difference is :" , fnum - snum)
+# elif op == "*":
+#     print("the product is :" ,fnum * snum)
+# else :
+#     print("Invalid operation")
+
+
+# ===== Modules in python =====
+# 1 )  math module
+import math
+print(math.sqrt(16))
+
+# 2 ) random module
+import random
+print(random.randint(1,100))
+
+# 3 )  keywoords module
+import keyword
+print(keyword.kwlist)
+
+# 4 ) datetime module 
+import datetime
+print(datetime.datetime.now())
+
+# if you want to know all the modules in python then you can use help function
+help('modules')
