23fe33adc3d7a8712d4ad0e72119f28607cf24ce
Harshit <srivastava0798@gmail.com>
2026-05-01 19:26:51 +0530

    min of three numbers

--- ifElse.py
+++ ifElse.py
@@ -35,3 +35,16 @@ elif email == "abc@gmail.com" and password != "abc123":
 # if email is wrong
 else:
     print("Invalid email")
+
+
+# min of 3 numbers
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+c = int(input("Enter third number :"))
+
+if a < b and a < c :
+    print("Minimum number is : " , a)
+elif b < a and b < c:
+    print("Minimum number is : " , b)
+else :
+    print("Minimum number is  :" ,c)
