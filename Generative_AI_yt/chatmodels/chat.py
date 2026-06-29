829a076fb8425b857ad4daa9f27aebf50af97178
Harshit <srivastava0798@gmail.com>
2026-06-29 19:55:11 +0530

    learn about temperature and max_tokens in LLM

--- Generative_AI_yt/chatmodels/chat.py
+++ Generative_AI_yt/chatmodels/chat.py
@@ -1,9 +1,13 @@
-from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatGroq(model="llama-3.1-8b-instant")
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7,
+    max_tokens=1000
+    )
 
 response = model.invoke("give me a paragraph on machine learning")
 
