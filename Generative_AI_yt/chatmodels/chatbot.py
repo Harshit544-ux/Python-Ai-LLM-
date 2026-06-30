d53cf5b6959e00593704388f7366e8f0b60c97d4
Harshit <srivastava0798@gmail.com>
2026-06-30 21:15:28 +0530

    feat: add multiple AI personality modes using system prompts

--- Generative_AI_yt/chatmodels/chatbot.py
+++ Generative_AI_yt/chatmodels/chatbot.py
@@ -1,17 +1,37 @@
+import os 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage , HumanMessage , AIMessage
 
 load_dotenv()
 
+
+print(os.getenv("GOOGLE_API_KEY"))
+
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=0.7,
+    temperature=0,
     )
+
+# deal with multiple ai mode
+print("choose your AI mode")
+print("press 1 for angry mode")
+print("press 2 for funny mode")
+print("press 3 for sad mode")
+
+choice = int(input("tell your response :- "))
+
+if choice == 1:
+    mode = "You are an angry AI agent. You respond aggressively and impateintly."
+elif choice == 2:
+      mode = "You are very funny AI agent. You respond with funny and jokes."
+elif choice == 3:
+       mode = "You are very sad  AI agent. You respond sadly and heartbreaking"
+
 
 # maintain the history in list message =[] 
 message = [
-    SystemMessage(content='You are a funny AI agent')
+    SystemMessage(content=mode)
 ]
 
 # welcome message
